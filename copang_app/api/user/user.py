96f860e9af9b9f2a4a3cc115420593768f4247af
전은형 <ooooo5h91@gmail.com>
2022-02-09 10:46:30 +0900

    이메일과 비밀번호 파라미터 받아보기

--- copang_app/api/user/user.py
+++ copang_app/api/user/user.py
@@ -7,7 +7,12 @@ from copang_app.models import Users
 class User(APIView):
     
     def post(self, request):
-           
+        
+        input_email = request.POST['email']
+        input_pw = request.POST['password']
+        
+        print(f'이메일 : {input_email}, 비밀번호 : {input_pw}')
+        
         return Response({
             'code' : 200,
             'message' : '임시 - 로그인 기능'
