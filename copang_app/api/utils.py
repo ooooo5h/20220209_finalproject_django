06ab4cedb47b66ae0aeccaa3529ad60a4b2ef68b
전은형 <ooooo5h91@gmail.com>
2022-02-09 11:39:52 +0900

    커스텀세팅에 저장한 비밀키와 알고리즘을 활용

--- copang_app/api/utils.py
+++ copang_app/api/utils.py
@@ -1,8 +1,11 @@
 # 사용자 정보를 가지고 토큰을 생성하기 
-
 import jwt
-
+import my_custom_settings
 
 def encode_token(user):
     
-    return jwt
+    return jwt.encode(
+        {'id':user.id, 'email':user.email, 'password': user.password_hashed, },
+        my_custom_settings.JWT_SECRET_KEY, # 비밀키를 뭘로 할건지
+        my_custom_settings.JWT_ALGORITHM, # 어떤 알고리즘
+    )
